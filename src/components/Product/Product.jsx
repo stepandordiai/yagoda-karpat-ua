061e22458082f4674587349c21bac40ff517216b
Stepan Dordiai <stepandordiai@gmail.com>
2025-04-25 22:39:37 +0200

    v3.5.1 Update data with new product and Product component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,82 +4,65 @@ import cameraIcon from "/assets/icons/camera.png";
 import "./Product.scss";
 
 const Product = ({ product }) => {
-    const { t } = useTranslation();
+	const { t } = useTranslation();
 
-    const { id, name, productImages } = product;
+	const { id, name, productImages } = product;
 
-    addEventListener("scroll", () => {
-        const slider = document.querySelectorAll(".my-swiper");
-        slider.forEach((slide) => {
-            if (window.innerHeight > slide.getBoundingClientRect().top + 200) {
-                slide.classList.add("active");
-            }
-        });
-        const productInfo = document.querySelectorAll(
-            ".product-info-container"
-        );
-        productInfo.forEach((product) => {
-            if (
-                window.innerHeight >
-                product.getBoundingClientRect().top + 200
-            ) {
-                product.classList.add("active");
-            }
-        });
-        const slider1 = document.querySelectorAll(".my-swiper-1");
-        slider1.forEach((slide) => {
-            if (window.innerHeight > slide.getBoundingClientRect().top + 200) {
-                slide.classList.add("active");
-            }
-        });
-        const productInfo1 = document.querySelectorAll(
-            ".product-info-container-1"
-        );
-        productInfo1.forEach((product) => {
-            if (
-                window.innerHeight >
-                product.getBoundingClientRect().top + 200
-            ) {
-                product.classList.add("active");
-            }
-        });
-    });
+	addEventListener("scroll", () => {
+		const slider = document.querySelectorAll(".my-swiper");
+		slider.forEach((slide) => {
+			if (window.innerHeight > slide.getBoundingClientRect().top + 200) {
+				slide.classList.add("active");
+			}
+		});
+		const productInfo = document.querySelectorAll(".product-info-container");
+		productInfo.forEach((product) => {
+			if (window.innerHeight > product.getBoundingClientRect().top + 200) {
+				product.classList.add("active");
+			}
+		});
+		const slider1 = document.querySelectorAll(".my-swiper-1");
+		slider1.forEach((slide) => {
+			if (window.innerHeight > slide.getBoundingClientRect().top + 200) {
+				slide.classList.add("active");
+			}
+		});
+		const productInfo1 = document.querySelectorAll(".product-info-container-1");
+		productInfo1.forEach((product) => {
+			if (window.innerHeight > product.getBoundingClientRect().top + 200) {
+				product.classList.add("active");
+			}
+		});
+	});
 
-    return (
-        <div className="product">
-            <div className="product__img-wrapper">
-                {productImages ? (
-                    <>
-                        <img
-                            className="product-img"
-                            src={productImages[0]}
-                            alt={name}
-                            loading="lazy"
-                        />
-                        <div className="img-qty">
-                            {productImages.length}
-                            <img
-                                src={cameraIcon}
-                                alt="Camera icon"
-                                loading="lazy"
-                            />
-                        </div>
-                    </>
-                ) : (
-                    <div className="no-img"></div>
-                )}
-            </div>
-            <div className="product__info-container">
-                <h4 className="product-name">{name}</h4>
-                <NavLink
-                    to={`/product-page/${id}`}
-                    className="product__info-btn"
-                >
-                    {t("products.show_more")}
-                </NavLink>
-            </div>
-        </div>
-    );
+	return (
+		<div className="product">
+			<div className="product__img-wrapper">
+				{productImages ? (
+					<>
+						<img
+							className="product-img"
+							src={productImages[0]}
+							alt={name}
+							loading="lazy"
+						/>
+						<div className="img-qty">
+							<img src={cameraIcon} alt="Camera icon" loading="lazy" />
+							<span>{productImages.length}</span>
+						</div>
+					</>
+				) : (
+					<div className="no-img"></div>
+				)}
+			</div>
+			<div className="product__info-container">
+				<h4 className="product-name">{name}</h4>
+				<NavLink to={`/product-page/${id}`} className="product__info-btn">
+					{t("products.show_more")}
+				</NavLink>
+			</div>
+		</div>
+	);
 };
 
 export default Product;
