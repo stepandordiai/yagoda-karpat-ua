0b54d7e0eaf9fe0969f930933c21237ef51e6830
Stepan Dordiai <stepandordiai@gmail.com>
2025-05-04 10:57:29 +0200

    v4.0.9 Update Contacts

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -24,7 +24,7 @@ const Contacts = () => {
 				</a>
 				<a
 					className="icon-container"
-					href="https://maps.app.goo.gl/U8hieofkGSuBgP7R9"
+					href="https://maps.app.goo.gl/ti6CzhjdpFoS8ytz7"
 					target="_blank"
 				>
 					<img src={pinIcon} alt="" />
@@ -84,11 +84,12 @@ const Contacts = () => {
 						{t("contacts.submit")}
 					</button>
 				</form>
-				<div className="google-map__wrapper">
-					<h2 className="google-map__title">{t("contacts.map_title")}</h2>
+				<div className="contacts__map-wrapper">
+					<h2 className="contacts__map-title">{t("contacts.map_title")}</h2>
 					<iframe
-						className="location-map"
-						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d666.0764004226559!2d23.74658175529479!3d48.10434021444485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47379f8eb1dc7f03%3A0x6e39e1068f4cb4a!2sYagoda%20Karpat%2C%20LLC!5e0!3m2!1sen!2sua!4v1722068818836!5m2!1sen!2sua"
+						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2664.3077328162444!2d23.7462056!3d48.1042991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47379f0033ca7d7d%3A0xf3d79b73a73bbce3!2z0K_Qs9C-0LTQsCDQmtCw0YDQv9Cw0YIg0KLQntCS!5e0!3m2!1sen!2scz!4v1746348694187!5m2!1sen!2scz"
+						className="contacts__map"
+						loading="lazy"
 					></iframe>
 				</div>
 			</div>
