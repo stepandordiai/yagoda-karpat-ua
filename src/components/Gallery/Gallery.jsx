e153fd39cdfef07d7294455dda294f6052cd5f09
Stepan Dordiai <stepandordiai@gmail.com>
2025-05-10 00:11:18 +0200

    v4.0.17 Update Gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -11,19 +11,24 @@ import "swiper/css/navigation";
 // import required modules
 import { Autoplay } from "swiper/modules";
 
+// Images are compressed
 import img1 from "/gallery/1.jpg";
 import img2 from "/gallery/2.jpg";
 import img3 from "/gallery/3.jpg";
 import img4 from "/gallery/4.jpg";
 import img5 from "/gallery/5.jpg";
-import img6 from "/gallery/6.jpg";
-import img7 from "/gallery/7.jpg";
 import styles from "./Gallery.module.scss";
 
 const Gallery = () => {
 	const { t } = useTranslation();
 
-	const images = [img1, img2, img3, img4, img5, img6, img7];
+	const galleryData = [
+		{ img: img1, date: "26/11/21" },
+		{ img: img2, date: "8/7/23" },
+		{ img: img3, date: "18/7/23" },
+		{ img: img4, date: "13/7/24" },
+		{ img: img5, date: "13/7/24" },
+	];
 
 	return (
 		<div className={styles["gallery"]}>
@@ -46,15 +51,14 @@ const Gallery = () => {
 				modules={[Autoplay]}
 				className={styles["mySwiper"]}
 			>
-				{images.map((img, index) => {
+				{galleryData.map(({ img, date }, index) => {
 					return (
-						<SwiperSlide key={index} className={styles["swiper-card"]}>
-							<img
-								className={styles["swiper-img"]}
-								src={img}
-								alt=""
-								loading="lazy"
-							/>
+						<SwiperSlide
+							key={index}
+							className={styles["swiper-card"]}
+							data-date={date}
+						>
+							<img src={img} alt="" loading="lazy" />
 						</SwiperSlide>
 					);
 				})}
