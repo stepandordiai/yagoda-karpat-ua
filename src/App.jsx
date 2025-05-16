20022dae1f779621e1149af0b5a065f016ac2dd3
Stepan Dordiai <stepandordiai@gmail.com>
2025-05-17 00:25:00 +0200

    v5.0.2 Update info

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import NavCurtain from "./components/NavCurtain/NavCurtain";
-
 import Home from "./pages/Home/Home";
-
 import ProductPage from "./pages/ProductPage/ProductPage";
 import ScrollToTop from "./utils/ScrollToTop";
 import "./i18n";
